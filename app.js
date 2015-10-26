fcdbe4a3d578e0644c616b7aa09002748e4095cd
sethdame <seth.e.dame@gmail.com>
2015-10-26 13:52:44 -0600

    binding

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('meanReddit', []);
 
-		app.controller('MainCtrl', [
-			'$scope',
-			function($scope){
+		app.controller('MainCtrl', function($scope){
 		  		$scope.test = 'Hello world!';
-}]);
+		  		$scope.posts = [
+						  {title: 'post 1', upvotes: 5},
+						  {title: 'post 2', upvotes: 2},
+						  {title: 'post 3', upvotes: 15},
+						  {title: 'post 4', upvotes: 9},
+						  {title: 'post 5', upvotes: 4}
+						];
+});
