6c76bc64aa8ca5a7f76e0d90d929b631ff246c10
sethdame <seth.e.dame@gmail.com>
2015-10-26 19:40:48 -0600

    posts

--- app.js
+++ app.js
@@ -27,16 +27,17 @@ var app = angular.module('meanReddit', ['ui.router', 'MainCtrl', 'posts', 'Posts
 		$scope.posts = posts.posts;
 		$scope.addPost = function(){
 			if (!$scope.title || $scope.title === '') { return; }
-				$scope.posts.push({title: $scope.title,
-									link: $scope.link,
-								 upvotes: 0,
-								 comments: [
-								 	{author: 'Joe', body: 'Cool post!', upvotes: 0},
-								 	{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-								 ]
-								});
-				$scope.title = '';
-				$scope.link = '';
+			$scope.posts.push({
+				title: $scope.title,
+				link: $scope.link,
+				upvotes: 0,
+				comments: [
+					{author: 'Joe', body: 'Cool post!', upvotes: 0},
+					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+					]
+				});
+			$scope.title = '';
+			$scope.link = '';
 		};
 		$scope.incrementUpvotes = function(post) {
 			post.upvotes += 1;
@@ -45,4 +46,13 @@ var app = angular.module('meanReddit', ['ui.router', 'MainCtrl', 'posts', 'Posts
 
 	app.controller('PostsCtrl', function($scope, $stateParams, posts) {
 		$scope.post = posts.post[$stateParams.id];
+		$scope.addComment = function() {
+			if ($scope.body === '') { return; }
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = '';
+		};
 	});
